54fa0f210363cc6567d75258c66a0e97f108997f
Emiel <emielonline@gmail.com>
2019-01-23 14:02:28 +0100

    Update interface

--- Interface/interface.py
+++ Interface/interface.py
@@ -35,14 +35,24 @@ def initial_query():
     return render_template('initial_query.html', query=text)
 
 
+@app.route('/advanced/', methods=['GET'])
+def advanced():
+    """
+    Function to display the main page.
+    This is the first thing you see when you load the website.
+    """
+    return render_template('advanced.html')
+
+MOCK_DATA = [{'title': 'Some clip', 'description': 'This is a cool video', 'shots': ['/static/Frames/frame0001.jpg']*5}] * 10
+MOCK_QUERIES = ['aap', 'hond', 'kat', 'cavia']
+
 @app.route('/results/', methods=['POST'])
 def results():
     """
     Show the results of the submitted query
     """
     text = request.form['textfield']
-    mock_data = [{'title': 'Some clip', 'description': 'This is a cool video', 'media': 'image_url'}] * 10
-    return render_template('results.html', query=text, results=mock_data)
+    return render_template('results.html', query=text, results=MOCK_DATA, related_queries=MOCK_QUERIES)
 
 ################################################################################
 # Running the website
