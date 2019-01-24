dbd91d6ab58901ca7241e0b3274d4181f680fae2
Arthur Barbosa Camara <acamara@Arthurs-MacBook-Pro.local>
2019-01-24 15:26:04 +0100

    gitignore

--- OCR Cleaning/clean_ocr.py
+++ OCR Cleaning/clean_ocr.py
@@ -1,6 +1,7 @@
 import tqdm
 import pandas as pd
 from cfuzzyset import cFuzzySet as FuzzySet
+import pickle
 from wordfreq import word_frequency
 import os
 import string
@@ -24,7 +25,7 @@ def load_dictionary():
         words.add(line.strip())
     return words
 
-dutch_vocabulary = load_dictionary()
+dutch_vocabulary = pickle.load(open("fuzzy_dict.pickle", 'rb'))
 
 
 def get_clean_word(x):
@@ -45,12 +46,50 @@ def get_clean_word(x):
             return candidate[1]
     candidate = dutch_vocabulary.get(x)[0]
     if candidate[0]< 0.6:
-        return None
+        return -1
     return candidate[1]
 
 def main():
     bad_chars =  string.punctuation+"'„”‘…»—“‘‚"
     translator = str.maketrans(bad_chars, ' '*len(bad_chars))
+    if len(sys.argv) > 2 and sys.argv[1] == "line":
+        home_folder = sys.argv[2]
+        print(home_folder)
+        outfolder = "data/processed_ocr/{}/".format(home_folder.split("/")[-1])
+        try:
+            os.mkdir(outfolder)
+        except Exception as e:
+            print(e)
+            pass
+        for counter, f in enumerate(tqdm.tqdm(os.listdir(home_folder))):
+            list_of_dicts = []
+            for line_number, line in enumerate(open(os.path.join(home_folder, f))):
+                line_dict = {}
+                if line_number == 0:
+                    header = line[:-1].split("\t")
+                    continue
+                l = line[:-1].split("\t")
+                if len(l) != 12:
+                    continue
+                text = l[-1].strip()
+                #empty string, low confidence, small font
+                if len(text.strip()) == 0 or int(l[-2]) <10 or int(l[8])<20:
+                    continue
+                line_dict = {k:v for k, v in zip(header, l)}
+                #remove punctuation marks
+                text = text.strip().translate(translator).strip()
+                #empty?
+                if len(text)<1:
+                    continue
+                #get words from dictionary
+                clean_text = " ".join([get_clean_word(x) for x in text.split() if get_clean_word(x)!=-1])
+                line_dict["clean_text"] = clean_text
+                list_of_dicts.append(line_dict.copy())
+            if len(list_of_dicts) == 0:
+                continue
+            final_df = pd.DataFrame(list_of_dicts)
+            final_df.to_csv(outfolder+f, sep="\t")
+        return
     home_folder = sys.argv[1]
     print(home_folder)
     outfolder = "data/processed_ocr/{}/".format(home_folder.split("/")[-1])
