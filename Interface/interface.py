7aad43375d1d5d3da68c8945ff09ea01c7c8adf3
Daan Odijk <daan.odijk@rtl.nl>
2019-01-24 17:03:41 +0100

    Load all concepts and BM25 weights.

--- Interface/interface.py
+++ Interface/interface.py
@@ -16,6 +16,19 @@ import elasticsearch
 app = Flask(__name__)
 es = elasticsearch.Elasticsearch()
 
+CONCEPT_FIELDS = ('places', 'places_attributes', 'places_environment', 'coco', 'coco_count', 'imagenet')
+
+try:
+    assert es.cluster.health(timeout='10s').get('status') == 'green'
+    MOCK_ES = False
+    all_docs = es.search('ictwi2019', size=10000, _source=CONCEPT_FIELDS, timeout='60s')["hits"]["hits"]
+except:
+    raise
+    app.logger.warn('Cannot connect to Elasticsearch, using mock data.')
+    MOCK_ES = True
+    with open('static/search.json') as f:
+        all_docs = json.load(f)["hits"]["hits"]
+
 ################################################################################
 # Webpage-related functions
 
@@ -27,57 +40,57 @@ def main_page():
     """
     return render_template('index.html')
 
-try:
-    assert es.cluster.health(timeout='10s').get('status') == 'green'
-    MOCK_ES = False
-except:
-    app.logger.warn('Cannot connect to Elasticsearch, using mock data.')
-    MOCK_ES = True
-
-def search(query):
+def search(query, weights):
     if MOCK_ES:
-        with open('static/search.json') as f:
-            search_data = json.load(f)
+        search_data = all_docs
     else:
-        search_data = es.search(q=query)
+        query = {"query": {"multi_match" : {
+            "query": query,
+            "fields": [f'{k}^{w["weight"]}' for k, w in weights.items()]
+        }}}
+        search_data = es.search(body=query)["hits"]["hits"]
 
     docs = []
-    for row in search_data["hits"]["hits"]:
+    for row in search_data:
         doc = row['_source']
         doc['id'] = row["_id"]
         doc["title"] = doc["title"].replace(" - RTL NIEUWS - YouTube", "")
         item_date_raw = doc["meta"]["datePublished"].split('-')
         doc['date'] = "{0}-{1}-{2}".format(item_date_raw[2], item_date_raw[1], item_date_raw[0])
-        doc['description'] = doc['description'].replace("Abonneer je GRATIS voor meer video\\xc2\\x92s: http://r.tl/1JBmAcsVolg nu LIVE het nieuws op: http://www.rtlnieuws.nlFacebook : https://www.facebook.com/rtlnieuwsnlTwitter : https://twitter.com/rtlnieuwsnl", "")
+        doc['description'] = doc['description'].split("Abonneer je GRATIS voor meer video")[0]
         doc['url'] = doc["meta"]["og:video:url"]
         doc['tags'] = [tag.capitalize() for tag in doc["meta"]["og:video:tag"][4:]]
         docs.append(doc)
     return docs
 
-weights = {
-    'BM25': {
-        'title': {'nl': 'Titel', 'weight': 50},
-        'description': {'nl': 'Beschrijving', 'weight': 20},
-        'tags': {'nl': 'Tags', 'weight': 10}
-    },
-    'Coco': {
-        'title': {'nl': 'Titel', 'weight': 50},
-        'description': {'nl': 'Beschrijving', 'weight': 20},
-        'tags': {'nl': 'Tags', 'weight': 10}
+def rank_concepts(query):
+    weights = {
+        'BM25': {
+            'title': {'nl': 'Titel', 'weight': 50},
+            'description': {'nl': 'Beschrijving', 'weight': 20},
+            'meta.og:video:tag': {'nl': 'Tags', 'weight': 10},
+            'clean_ocr': {'nl': 'OCR', 'weight': 10}
+        },
+        'Coco': {
+            'cow': {'nl': 'Koe', 'weight': 50},
+            'person': {'nl': 'Persoon', 'weight': 20},
+            'tree': {'nl': 'Boom', 'weight': 10}
+        }
     }
-}
+    return weights
 
 @app.route('/results/', methods=['POST'])
 def results():
     """
     Show the results of the submitted query
     """
-    text = request.form['textfield']
+    query = request.form['textfield']
+    weights = rank_concepts(query)
     for category in weights:
         for key, value in weights[category].items():
-            if key in request.form:
-                value['weight'] = request.form[key]
-    return render_template('results.html', query=text, results=search(text), weights=weights)
+            if f'{category}/{key}' in request.form:
+                value['weight'] = request.form[f'{category}/{key}']
+    return render_template('results.html', query=query, results=search(query, weights['BM25']), weights=weights)
 
 ################################################################################
 # Running the website
