f7a697c585e3bc822fb0d9281632ebbd7c79838d
Maurits van der Goes <maurits.van.der.Goes@rtl.nl>
2019-01-24 15:14:31 +0100

    sliders added

--- Interface/interface.py
+++ Interface/interface.py
@@ -52,19 +52,32 @@ with open('static/search.json') as f:
 MOCK_DATA = []
 for row in search_data["hits"]["hits"]:
     title = row["_source"]["title"].replace(" - RTL NIEUWS - YouTube", "")
-    description = row["_source"]['description'].decode('utf8').encode('ascii', errors='ignore')
+    description = row["_source"]['description'].decode('utf8')
     item_date_raw = row["_source"]["meta"]["datePublished"].split('-')
     item_date = "{0}-{1}-{2}".format(item_date_raw[2], item_date_raw[1], item_date_raw[0])
-    chapeau = row["_source"]["meta"]["keywords"].split(', ')[4].capitalize()
     description = description.replace("Abonneer je GRATIS voor meer video\\xc2\\x92s: http://r.tl/1JBmAcsVolg nu LIVE het nieuws op: http://www.rtlnieuws.nlFacebook : https://www.facebook.com/rtlnieuwsnlTwitter : https://twitter.com/rtlnieuwsnl", "")
     MOCK_DATA.append({'title': title,
                       'description': description,
+                      'id': row["_id"],
                       'url': row["_source"]["meta"]["og:video:url"],
                       'date': item_date,
-                      'chapeau': chapeau,
+                      'tags': [tag.capitalize() for tag in row["_source"]["meta"]["og:video:tag"][4:]],
                       'shots': ['/static/Frames/frame0001.jpg']*5})
 
 MOCK_QUERIES = ['aap', 'hond', 'kat', 'cavia']
+
+weights = {
+    'BM25': {
+        'title': {'nl': 'Titel', 'weight': 50},
+        'description': {'nl': 'Beschrijving', 'weight': 20},
+        'tags': {'nl': 'Tags', 'weight': 10}
+    },
+    'Coco': {
+        'title': {'nl': 'Titel', 'weight': 50},
+        'description': {'nl': 'Beschrijving', 'weight': 20},
+        'tags': {'nl': 'Tags', 'weight': 10}
+    }
+}
 
 @app.route('/results/', methods=['POST'])
 def results():
@@ -72,7 +85,11 @@ def results():
     Show the results of the submitted query
     """
     text = request.form['textfield']
-    return render_template('results.html', query=text, results=MOCK_DATA, related_queries=MOCK_QUERIES)
+    for category in weights:
+        for key, value in weights[category].items():
+            if key in request.form:
+                value['weight'] = request.form[key]
+    return render_template('results.html', query=text, results=MOCK_DATA, related_queries=MOCK_QUERIES, weights=weights)
 
 ################################################################################
 # Running the website
