6e850e25565f07cee1059cf8c0a3253ae72dac72
Daan Odijk <daan.odijk@rtl.nl>
2019-01-24 15:55:30 +0100

    Integration with Elasticsearch

--- Interface/interface.py
+++ Interface/interface.py
@@ -1,6 +1,7 @@
 # Flask-related imports:
 from flask import Flask, request, render_template
 import json
+import elasticsearch
 
 ################################################################################
 # Load data
@@ -13,6 +14,7 @@ import json
 ################################################################################
 # Set up app:
 app = Flask(__name__)
+es = elasticsearch.Elasticsearch()
 
 ################################################################################
 # Webpage-related functions
@@ -25,46 +27,32 @@ def main_page():
     """
     return render_template('index.html')
 
+try:
+    assert es.cluster.health(timeout='10s').get('status') == 'green'
+    MOCK_ES = False
+except:
+    app.logger.warn('Cannot connect to Elasticsearch, using mock data.')
+    MOCK_ES = True
 
-@app.route('/initial-query/', methods=['POST'])
-def initial_query():
-    """
-    Show the interpretation of the user query.
-    Users can still modify their query.
-    """
-    text = request.form['textfield']
-    return render_template('initial_query.html', query=text)
+def search(query):
+    if MOCK_ES:
+        with open('static/search.json') as f:
+            search_data = json.load(f)
+    else:
+        search_data = es.search(q=query)
 
-
-@app.route('/advanced/', methods=['GET'])
-def advanced():
-    """
-    Function to display the main page.
-    This is the first thing you see when you load the website.
-    """
-    return render_template('advanced.html')
-
-MOCK_DATA = [{'title': 'Some clip', 'description': 'This is a cool video', 'shots': ['/static/Frames/frame0001.jpg']*5}] * 10
-
-with open('static/search.json') as f:
-    search_data = json.load(f)
-
-MOCK_DATA = []
-for row in search_data["hits"]["hits"]:
-    title = row["_source"]["title"].replace(" - RTL NIEUWS - YouTube", "")
-    description = row["_source"]['description'].decode('utf8')
-    item_date_raw = row["_source"]["meta"]["datePublished"].split('-')
-    item_date = "{0}-{1}-{2}".format(item_date_raw[2], item_date_raw[1], item_date_raw[0])
-    description = description.replace("Abonneer je GRATIS voor meer video\\xc2\\x92s: http://r.tl/1JBmAcsVolg nu LIVE het nieuws op: http://www.rtlnieuws.nlFacebook : https://www.facebook.com/rtlnieuwsnlTwitter : https://twitter.com/rtlnieuwsnl", "")
-    MOCK_DATA.append({'title': title,
-                      'description': description,
-                      'id': row["_id"],
-                      'url': row["_source"]["meta"]["og:video:url"],
-                      'date': item_date,
-                      'tags': [tag.capitalize() for tag in row["_source"]["meta"]["og:video:tag"][4:]],
-                      'shots': ['/static/Frames/frame0001.jpg']*5})
-
-MOCK_QUERIES = ['aap', 'hond', 'kat', 'cavia']
+    docs = []
+    for row in search_data["hits"]["hits"]:
+        doc = row['_source']
+        doc['id'] = row["_id"]
+        doc["title"] = doc["title"].replace(" - RTL NIEUWS - YouTube", "")
+        item_date_raw = doc["meta"]["datePublished"].split('-')
+        doc['date'] = "{0}-{1}-{2}".format(item_date_raw[2], item_date_raw[1], item_date_raw[0])
+        doc['description'] = doc['description'].replace("Abonneer je GRATIS voor meer video\\xc2\\x92s: http://r.tl/1JBmAcsVolg nu LIVE het nieuws op: http://www.rtlnieuws.nlFacebook : https://www.facebook.com/rtlnieuwsnlTwitter : https://twitter.com/rtlnieuwsnl", "")
+        doc['url'] = doc["meta"]["og:video:url"]
+        doc['tags'] = [tag.capitalize() for tag in doc["meta"]["og:video:tag"][4:]]
+        docs.append(doc)
+    return docs
 
 weights = {
     'BM25': {
@@ -89,7 +77,7 @@ def results():
         for key, value in weights[category].items():
             if key in request.form:
                 value['weight'] = request.form[key]
-    return render_template('results.html', query=text, results=MOCK_DATA, related_queries=MOCK_QUERIES, weights=weights)
+    return render_template('results.html', query=text, results=search(text), weights=weights)
 
 ################################################################################
 # Running the website
